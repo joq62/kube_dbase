3a66687d6b2878b4a96ddaf22cb039a70430c569
joq62 <joakim.leche@gmail.com>
2021-08-18 00:43:04 +0200

    ud

--- src/db_pod.erl
+++ src/db_pod.erl
@@ -7,10 +7,10 @@
 -define(TABLE,pod_status).
 -define(RECORD,pod_status).
 -record(pod_status,{
-		    pod_id,
+		    reference,
 		    pod_node,
 		    pod_dir,
-		    app_state, %[{app,loaded|started,PodSpec}]
+		    pod_specs, 
 		    host_node,
 		    created
 		   }).
@@ -22,24 +22,24 @@ create_table()->
 				{type,bag}]),
     mnesia:wait_for_tables([?TABLE], 20000).
 
-create(PodId,PodNode,PodDir,AppState,HostNode,Created)->
+create(Reference,PodNode,PodDir,PodSpecs,HostNode,Created)->
     Record=#?RECORD{
+		    reference=Reference,
 		    pod_node=PodNode,
-		    pod_id=PodId,
 		    pod_dir=PodDir,
-		    app_state=AppState, 
+		    pod_specs=PodSpecs, 
 		    host_node=HostNode,
 		    created=Created
 		   },
     F = fun() -> mnesia:write(Record) end,
     mnesia:transaction(F).
 
-delete(PodNode)->
+delete(Reference)->
     F=fun()->
 		   case do(qlc:q([X || X <- mnesia:table(?TABLE),
-				       X#?RECORD.pod_node==PodNode])) of
+				       X#?RECORD.reference==Reference])) of
 		       []->
-			   mnesia:abort({error,[ticket,"eexist",[PodNode]]});
+			   mnesia:abort({error,[ticket,"eexist",[Reference]]});
 		       [R]->  
 			   mnesia:delete_object(R)
 		   end
@@ -49,35 +49,37 @@ delete(PodNode)->
     
 read_all() ->
     Z=do(qlc:q([X || X <- mnesia:table(?TABLE)])),
-    [{PodId,PodNode,PodDir,AppState,HostNode,Created}||
-	{?RECORD,PodId,PodNode,PodDir,AppState,HostNode,Created}<-Z].
+    [{Reference,PodNode,PodDir,PodSpecs,HostNode,Created}||
+	{?RECORD,Reference,PodNode,PodDir,PodSpecs,HostNode,Created}<-Z].
 
 %% 
-dir(PodNode)->
-    read(PodNode,pod_dir).
-app_state(PodNode)->
-    read(PodNode,app_state).
-host_node(PodNode)->
-    read(PodNode,host_node).
-created(PodNode)->
-    read(PodNode,created).
+node(Reference)->
+    read(Reference,pod_node).
+dir(Reference)->
+    read(Reference,pod_dir).
+pod_specs(Reference)->
+    read(Reference,pod_specs).
+host_node(Reference)->
+    read(Reference,host_node).
+created(Reference)->
+    read(Reference,created).
 
-read(PodNode,Key)->
+read(Reference,Key)->
     Result=case do(qlc:q([X || X <- mnesia:table(?TABLE),
-			       X#?RECORD.pod_node==PodNode])) of
+			       X#?RECORD.reference==Reference])) of
 	       []->
-		   {error,[ticket,"eexist",[PodNode]]};
+		   {error,[ticket,"eexist",[Reference]]};
 	       [R]->
 		   
 		   case Key of
-		       pod_id->
-			   R#?RECORD.pod_id;
+		       reference->
+			   R#?RECORD.reference;
 		       pod_node->
 			   R#?RECORD.pod_node;
 		       pod_dir->
 			   R#?RECORD.pod_dir;
-		       app_state->
-			   R#?RECORD.app_state;
+		       pod_specs->
+			   R#?RECORD.pod_specs;
 		       host_node->
 			   R#?RECORD.host_node;
 		       created->
@@ -88,43 +90,32 @@ read(PodNode,Key)->
 	   end,
     Result.
 
-add_app(PodNode,App,Stated,PodSpec)->
+add_spec(Reference,PodSpec)->
     F = fun() -> 
 		RecordList=do(qlc:q([X || X <- mnesia:table(?TABLE),
-					  X#?RECORD.pod_node==PodNode])),
+					  X#?RECORD.reference==Reference])),
 		case RecordList of
 		    []->
-			%_Result={error,[ticket,"eexist",[PodNode]]},
-			mnesia:abort(?TABLE);
+			mnesia:abort({error,[ticket,"eexist",[Reference,RecordList]]});
 		    [S1]->
-			case lists:keymember(App,1,S1#?RECORD.app_state) of
-			    true->
-			%	_Result={error,[ticket,"exists",[App,PodNode,PodSpec]]},
-				mnesia:abort(?TABLE);				
-			    false->
-				AppState=[{App,Stated,PodSpec}|S1#?RECORD.app_state],
-				NewRecord=S1#?RECORD{app_state=AppState},
-			%	_Result={ok,[AppState]},
-				
-				mnesia:delete_object(S1),
-				mnesia:write(NewRecord)
-			end
+			UpdatedPodSpecs=[PodSpec|lists:delete(PodSpec,S1#?RECORD.pod_specs)],
+			NewRecord=S1#?RECORD{pod_specs=UpdatedPodSpecs},
+			mnesia:delete_object(S1),
+			mnesia:write(NewRecord)
 		end
 	end,
     mnesia:transaction(F).
 
-remove_app(PodNode,App)->
+remove_spec(Reference,PodSpec)->
     F = fun() -> 
 		RecordList=do(qlc:q([X || X <- mnesia:table(?TABLE),
-					  X#?RECORD.pod_node==PodNode])),
+					   X#?RECORD.reference==Reference])),
 		case RecordList of
 		    []->
-			%_Result={error,[ticket,"eexist",[PodNode]]},
-			mnesia:abort(?TABLE);
+			mnesia:abort({error,[ticket,"eexist",[Reference,RecordList]]});
 		    [S1]->
-			AppState=lists:keydelete(App,1,S1#?RECORD.app_state),
-			NewRecord=S1#?RECORD{app_state=AppState},
-			%_Result={ok,[AppState]},
+			UpdatedPodSpecs=lists:delete(PodSpec,S1#?RECORD.pod_specs),
+			NewRecord=S1#?RECORD{pod_specs=UpdatedPodSpecs},
 			mnesia:delete_object(S1),
 			mnesia:write(NewRecord)
 		end
